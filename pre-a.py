3b80b1f9a06a5478009c3a670a1d33487fdacc29
Amui <chunxue-chen@163.com>
2018-03-15 17:23:00 +0800

    data

--- pre-a.py
+++ pre-a.py
@@ -14,9 +14,11 @@ parser.add_argument('sparse_path', type=str)
 args = vars(parser.parse_args())
 
 #These features are dense enough (they appear in the dataset more than 4 million times), so we include them in GBDT
+#26
 #target_cat_feats = ['C9-a73ee510', 'C22-', 'C17-e5ba7672', 'C26-', 'C23-32c7478e', 'C6-7e0ccccf', 'C14-b28479f6', 'C19-21ddcdc9', 'C14-07d13a8f', 'C10-3b08e48b', 'C6-fbad5c96', 'C23-3a171ecb', 'C20-b1252a9d', 'C20-5840adea', 'C6-fe6b92e5', 'C20-a458ea53', 'C14-1adce6ef', 'C25-001f3601', 'C22-ad3062eb', 'C17-07c540c4', 'C6-', 'C23-423fab69', 'C17-d4bb7bd8', 'C2-38a947a1', 'C25-e8b83407', 'C9-7cc72ec2']
 #target_cat_feats = get_ctg_feats()
 
+#69
 target_cat_feats=['C1-8268', 'C1-3295', 'C1-5927', 'C1-2279', 'C1-3026', 'C1-6272', 'C1-5985', 'C1-3040', 
                   'C2-8277336076276184272', 'C2-5755694407684602296', 'C2-509660095530134768', 'C2-5799347067982556520', 'C2-7258015885215914736', 
                   'C3-1755', 'C3-1594', 'C3-1144', 'C3-92', 'C3-160', 'C3-1533', 'C3-427', 'C3-1049', 'C3-1492', 'C3-1836', 'C3-1322', 'C3-844', 'C3-1584', 'C3-1609', 'C3-834', 'C3-261', 'C3-413',
